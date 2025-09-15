57b8f59441f4d7e5bb73c5f2611c8f5d9156e9bc
BhavyasriTata <bhavyasritata@gmail.com>
2025-09-15 20:02:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,19 +133,7 @@ elif choice == "Resources":
     elif resource_type == "Texts":
         st.download_button(
             "📥 Download Wellness Guide (PDF)",
-            data=" Self-care means taking the time to do things that help you live well and improve both your physical health and mental health. This can help you manage stress, lower your risk of illness, and increase your energy. Even small acts of self-care in your daily life can have a big impact.
-
-Here are some self-care tips:
-
-Get regular exercise. Just 30 minutes of walking every day can boost your mood and improve your health. Small amounts of exercise add up, so don’t be discouraged if you can’t do 30 minutes at one time.
-Eat healthy, regular meals and stay hydrated. A balanced diet and plenty of water can improve your energy and focus throughout the day. Pay attention to your intake of caffeine and alcohol and how they affect your mood and well-being—for some, decreasing caffeine and alcohol consumption can be helpful.
-Make sleep a priority. Stick to a schedule, and make sure you’re getting enough sleep. Blue light from devices and screens can make it harder to fall asleep, so reduce blue light exposure from your phone or computer before bedtime.
-Try a relaxing activity. Explore relaxation or wellness programs or apps, which may incorporate meditation, muscle relaxation, or breathing exercises. Schedule regular times for these and other healthy activities you enjoy, such as listening to music, reading, spending time in nature, and engaging in low-stress hobbies.
-Set goals and priorities. Decide what must get done now and what can wait. Learn to say “no” to new tasks if you start to feel like you’re taking on too much. Try to appreciate what you have accomplished at the end of the day.
-Practice gratitude. Remind yourself daily of things you are grateful for. Be specific. Write them down or replay them in your mind.
-Focus on positivity. Identify and challenge your negative and unhelpful thoughts.
-Stay connected. Reach out to friends or family members who can provide emotional support and practical help.
-Self-care looks different for everyone, and it is important to find what you need and enjoy. It may take trial and error to discover what works best for you. ",
+            data=" This a sample data. ",
             file_name="wellness_guide.txt",
         )
 
@@ -193,6 +181,7 @@ elif choice == "Admin Dashboard":
     st.altair_chart(chart, use_container_width=True)
 
     st.metric("Total Resources Played", plays)
+
 
 
 
