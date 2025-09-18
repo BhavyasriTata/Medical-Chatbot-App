37c7c1d0b425e38ebc61ae49b6e88f6fca028c61
BhavyasriTata <bhavyasritata@gmail.com>
2025-09-18 21:12:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if USE_OPENAI:
 # ---------- CONFIG ----------
 DB_PATH = "mental_platform.db"
 FERNET_KEY = os.getenv("FERNET_KEY")  # optional — base64 key from Fernet.generate_key()
-MOD_PASSWORD = os.getenv("MOD_PASSWORD", "pass123")  # Change in deployment
+MOD_PASSWORD = os.getenv("MOD_PASSWORD", "modpass123")  # Change in deployment
 EMERGENCY_HELPLINE =  """Please reach out for immediate help. You are not alone.\n\n
                 📞 National Suicide Prevention Lifeline (India): 9152987821\n
                 📞 KIRAN Mental Health Helpline: 1800-599-0019\n\n
@@ -951,6 +951,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
